4e83556f99ddb10e5e455df6728b417cdc0bbfa1
Haochen Zhang <hz112@rice.edu>
2026-04-07 01:27:07 -0500

    0406

--- cond_imagen_few_pretrain.sh
+++ cond_imagen_few_pretrain.sh
@@ -1,14 +1,14 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 python run.py \
---subset_p 1.0 \
---ddp \
---wandb \
---wandb_project CondImagenFewPretrain \
---config ./configs/conditional_pretrain/pretrain.yaml \
---no_test_model
-
-#python run.py \
+#CUDA_VISIBLE_DEVICES=4,5,6,7 python run.py \
 #--subset_p 1.0 \
+#--ddp \
 #--wandb \
 #--wandb_project CondImagenFewPretrain \
 #--config ./configs/conditional_pretrain/pretrain.yaml \
 #--no_test_model
+
+CUDA_VISIBLE_DEVICES=4 python run.py \
+--subset_p 1.0 \
+--wandb \
+--wandb_project CondImagenFewPretrain \
+--config ./configs/conditional_pretrain/pretrain.yaml \
+--no_test_model
