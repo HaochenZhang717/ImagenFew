31a8e61b38114ef2f0af489c71ddeb0b2ea690d2
Haochen Zhang <hz112@rice.edu>
2026-04-07 18:16:09 -0500

    0407

--- scripts/export_vae_latent_finetune_dataset.sh
+++ scripts/export_vae_latent_finetune_dataset.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 python export_vae_latents_finetune_dataset.py \
-  --vae-config ../configs/pretrain/vae_pretrain.yaml \
-  --vae-ckpt ../logs/vae_pretrain/52131940-4465-42cb-85b4-93bd7f5ee944/MultiScaleVAE \
-  --dataset-config-dir ../configs/DiffusionTS \
-  --output-dir ../logs/vae_latents/finetune_dataset \
+  --vae-config ./configs/pretrain/vae_pretrain.yaml \
+  --vae-ckpt ./logs/vae_pretrain/52131940-4465-42cb-85b4-93bd7f5ee944/MultiScaleVAE \
+  --dataset-config-dir ./configs/DiffusionTS \
+  --output-dir ./logs/vae_latents/finetune_dataset \
   --batch-size 256
