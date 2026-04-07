a2a961069127d48730dbc4aef6a7056dadf862af
Haochen Zhang <hz112@rice.edu>
2026-04-07 01:08:41 -0500

    0406

--- cond_imagen_few_pretrain.sh
+++ cond_imagen_few_pretrain.sh
@@ -1,6 +1,14 @@
-CUDA_VISIBLE_DEVICES=0 python run.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python run.py \
 --subset_p 1.0 \
+--ddp \
 --wandb \
 --wandb_project CondImagenFewPretrain \
 --config ./configs/conditional_pretrain/pretrain.yaml \
 --no_test_model
+
+#python run.py \
+#--subset_p 1.0 \
+#--wandb \
+#--wandb_project CondImagenFewPretrain \
+#--config ./configs/conditional_pretrain/pretrain.yaml \
+#--no_test_model
