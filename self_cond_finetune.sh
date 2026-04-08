8d8495e4174f4cc243076298d1d0a8c9041f9467
Haochen Zhang <hz112@rice.edu>
2026-04-08 09:31:39 -0500

    0407

--- self_cond_finetune.sh
+++ self_cond_finetune.sh
@@ -5,14 +5,14 @@ CUDA_VISIBLE_DEVICES=0 python run.py \
 --config ./configs/self_cond_finetune/ETTh2.yaml
 
 
-CUDA_VISIBLE_DEVICES=1 python run.py \
+CUDA_VISIBLE_DEVICES=2 python run.py \
 --subset_p 1.0 \
 --wandb \
 --wandb_project SelfConditionalGenerationFinetune \
 --config ./configs/self_cond_finetune/AirQuality.yaml
 
 
-CUDA_VISIBLE_DEVICES=2 python run.py \
+CUDA_VISIBLE_DEVICES=3 python run.py \
 --subset_p 1.0 \
 --wandb \
 --wandb_project SelfConditionalGenerationFinetune \
