18eb9010397c818a10362a7701c8c5d886c2ee31
Haochen Zhang <hz112@rice.edu>
2026-04-08 11:41:50 -0500

    0407

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -6,21 +6,21 @@ from tqdm import tqdm
 
 def evaluate_model_uncond(real_sig, gen_sig, dataset, device, eval_metrics=['disc'], metric_iteration=10, base_path=None):
 
-    if 'disc' in eval_metrics:
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
-        disc_res = list()
-        for _ in tqdm(range(metric_iteration), desc='Discriminative score evaluation'):
-            dsc = discriminative_score_metrics(real_sig, gen_sig, device)
-            disc_res.append(dsc)
-        disc_mean, disc_std = np.round(np.mean(disc_res), 4), np.round(np.std(disc_res), 4)
-    else:
-        disc_mean, disc_std = -1, -1
-
-    if 'contextFID' in eval_metrics:
-        context_fid = Context_FID(real_sig, gen_sig, dataset, device, base_path)
-    else:
-        context_fid = -1
+    # if 'disc' in eval_metrics:
+    #     torch.backends.cudnn.deterministic = True
+    #     torch.backends.cudnn.benchmark = False
+    #     disc_res = list()
+    #     for _ in tqdm(range(metric_iteration), desc='Discriminative score evaluation'):
+    #         dsc = discriminative_score_metrics(real_sig, gen_sig, device)
+    #         disc_res.append(dsc)
+    #     disc_mean, disc_std = np.round(np.mean(disc_res), 4), np.round(np.std(disc_res), 4)
+    # else:
+    #     disc_mean, disc_std = -1, -1
+    #
+    # if 'contextFID' in eval_metrics:
+    #     context_fid = Context_FID(real_sig, gen_sig, dataset, device, base_path)
+    # else:
+    #     context_fid = -1
 
     if 'pred' in eval_metrics:
         from metrics.predictive_metrics_pytorch import predictive_score_metrics
@@ -31,7 +31,7 @@ def evaluate_model_uncond(real_sig, gen_sig, dataset, device, eval_metrics=['dis
         pred_mean, pred_std = np.round(np.mean(predictive_score), 4), np.round(np.std(predictive_score), 4)
     else:
         pred_mean, pred_std= -1, -1
-
+    breakpoint()
     return {
         f'disc_mean':disc_mean,
         f'disc_std':disc_std,
