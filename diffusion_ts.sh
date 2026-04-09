2b673ab42af4517a3d9e9ef6b93d07c7332effb7
Haochen Zhang <hz112@rice.edu>
2026-04-08 21:17:48 -0500

    0408

--- diffusion_ts.sh
+++ diffusion_ts.sh
@@ -1,22 +1,84 @@
-CUDA_VISIBLE_DEVICES=0 python run.py \
+export CUDA_VISIBLE_DEVICES=0
+
+#python run.py \
+#--subset_p 1.0 \
+#--wandb \
+#--wandb_project DiffusionTS \
+#--config ./configs/DiffusionTS/AirQuality.yaml
+
+
+#python run.py \
+#--subset_p 1.0 \
+#--wandb \
+#--wandb_project DiffusionTS \
+#--config ./configs/DiffusionTS/ECG200.yaml
+
+
+#python run.py \
+#--subset_p 1.0 \
+#--wandb \
+#--wandb_project DiffusionTS \
+#--config ./configs/DiffusionTS/ETTh2.yaml
+
+
+python run.py \
 --subset_p 1.0 \
 --wandb \
---wandb_project Diffusion-TS \
---config ./configs/DiffusionTS/AirQuality.yaml
+--wandb_project DiffusionTS \
+--config ./configs/DiffusionTS/ETTm1.yaml
 
 
-CUDA_VISIBLE_DEVICES=0 python run.py \
+python run.py \
 --subset_p 1.0 \
 --wandb \
---wandb_project Diffusion-TS \
---config ./configs/DiffusionTS/ETTh2.yaml
+--wandb_project DiffusionTS \
+--config ./configs/DiffusionTS/ETTm2.yaml
 
 
-CUDA_VISIBLE_DEVICES=0 python run.py \
+python run.py \
 --subset_p 1.0 \
 --wandb \
---wandb_project Diffusion-TS \
+--wandb_project DiffusionTS \
+--config ./configs/DiffusionTS/ILI.yaml
+
+
+python run.py \
+--subset_p 1.0 \
+--wandb \
+--wandb_project DiffusionTS \
 --config ./configs/DiffusionTS/Mujoco.yaml
 
 
+python run.py \
+--subset_p 1.0 \
+--wandb \
+--wandb_project DiffusionTS \
+--config ./configs/DiffusionTS/SaugeenRiverFlow.yaml
 
+
+python run.py \
+--subset_p 1.0 \
+--wandb \
+--wandb_project DiffusionTS \
+--config ./configs/DiffusionTS/SelfRegulationSCP1.yaml
+
+
+python run.py \
+--subset_p 1.0 \
+--wandb \
+--wandb_project DiffusionTS \
+--config ./configs/DiffusionTS/Sine.yaml
+
+
+python run.py \
+--subset_p 1.0 \
+--wandb \
+--wandb_project DiffusionTS \
+--config ./configs/DiffusionTS/StarLightCurves.yaml
+
+
+python run.py \
+--subset_p 1.0 \
+--wandb \
+--wandb_project DiffusionTS \
+--config ./configs/DiffusionTS/Weather.yaml
