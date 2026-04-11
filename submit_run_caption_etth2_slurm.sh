e31891e0281ee9af3b5340bff7ed9c4431e3dab7
Haochen Zhang <hz112@rice.edu>
2026-04-11 01:35:15 -0500

    0411

--- submit_run_caption_etth2_slurm.sh
+++ submit_run_caption_etth2_slurm.sh
@@ -13,3 +13,4 @@ sbatch --time="${SLURM_TIME:-2-00:00:00}" run_caption_etth2_part2_slurm.sh
 
 CONDA_ENV="${CONDA_ENV:-vlm}" \
 sbatch --time="${SLURM_TIME:-2-00:00:00}" run_caption_etth2_part3_slurm.sh
+
