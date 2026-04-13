42de0d481a7c7bc00a155bdee3f8133f4d3f9d72
Haochen Zhang <hz112@rice.edu>
2026-04-13 13:14:47 -0500

    debug text-conditioned generator

--- CondImagenFew_eval.sh
+++ CondImagenFew_eval.sh
@@ -2,5 +2,5 @@ CONDA_ENV=vlm \
 CUDA_VISIBLE_DEVICES=2 \
 MODEL_CKPT=./logs/CondImagenFewFinetune/ETTh2/19c5b45b-e429-4f16-96dc-180ad93dd2df/ImagenFewCrossAttention.pt \
 PRIOR_CKPT=./logs/diffusion_prior/time_shift_sweep/ETTh2/tds_1p0/diffusion_prior_latest.pt \
-CONFIG= ./configs/conditional_imagen_few/ETTh2.yaml
+CONFIG=./configs/conditional_imagen_few/ETTh2.yaml \
 bash evaluate_imagenfew_cross_attention_prior_local.sh
