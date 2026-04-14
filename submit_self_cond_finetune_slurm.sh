d7072e2f7f6397e50c1b72486169fe05635f521d
Haochen Zhang <hz112@rice.edu>
2026-04-14 04:59:04 -0500

    0413

--- submit_self_cond_finetune_slurm.sh
+++ submit_self_cond_finetune_slurm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-CONDA_ENV="${CONDA_ENV:-vlm}" \
-sbatch --time="${SLURM_TIME:-2-00:00:00}" self_cond_finetune_etth2_slurm.sh
+#CONDA_ENV="${CONDA_ENV:-vlm}" \
+#sbatch --time="${SLURM_TIME:-2-00:00:00}" self_cond_finetune_etth2_slurm.sh
 
 CONDA_ENV="${CONDA_ENV:-vlm}" \
 sbatch --time="${SLURM_TIME:-2-00:00:00}" self_cond_finetune_airquality_slurm.sh
