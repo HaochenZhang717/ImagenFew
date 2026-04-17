8179d0849410437af3ffabe5bed39fba6b84e50e
Haochen Zhang <hz112@rice.edu>
2026-04-17 15:41:52 +0800

    0416

--- scripts_refine/submit_refine_finetune.sh
+++ scripts_refine/submit_refine_finetune.sh
@@ -12,9 +12,9 @@ AIRQUALITY_CONFIG="${AIRQUALITY_CONFIG:-$ROOT_DIR/configs/refine/AirQuality.yaml
 MUJOCO_CONFIG="${MUJOCO_CONFIG:-$ROOT_DIR/configs/refine/mujoco.yaml}"
 WANDB_PROJECT_VALUE="${WANDB_PROJECT:-ImagenFewRefine}"
 
-ETTH2_JOB=$(CONDA_ENV="$CONDA_ENV_VALUE" WANDB_PROJECT="$WANDB_PROJECT_VALUE" CONFIG="$ETTH2_CONFIG" sbatch --parsable --time="$SLURM_TIME_VALUE" "$ROOT_DIR/scripts_refine/train_refine_finetune.sh")
-AIRQUALITY_JOB=$(CONDA_ENV="$CONDA_ENV_VALUE" WANDB_PROJECT="$WANDB_PROJECT_VALUE" CONFIG="$AIRQUALITY_CONFIG" sbatch --parsable --time="$SLURM_TIME_VALUE" "$ROOT_DIR/scripts_refine/train_refine_finetune.sh")
-MUJOCO_JOB=$(CONDA_ENV="$CONDA_ENV_VALUE" WANDB_PROJECT="$WANDB_PROJECT_VALUE" CONFIG="$MUJOCO_CONFIG" sbatch --parsable --time="$SLURM_TIME_VALUE" "$ROOT_DIR/scripts_refine/train_refine_finetune.sh")
+ETTH2_JOB=$(CONDA_ENV="$CONDA_ENV_VALUE" WANDB_PROJECT="$WANDB_PROJECT_VALUE" CONFIG="$ETTH2_CONFIG" sbatch --parsable --time="$SLURM_TIME_VALUE" "$ROOT_DIR/scripts_refine/train_refine_etth2.sh")
+AIRQUALITY_JOB=$(CONDA_ENV="$CONDA_ENV_VALUE" WANDB_PROJECT="$WANDB_PROJECT_VALUE" CONFIG="$AIRQUALITY_CONFIG" sbatch --parsable --time="$SLURM_TIME_VALUE" "$ROOT_DIR/scripts_refine/train_refine_airquality.sh")
+MUJOCO_JOB=$(CONDA_ENV="$CONDA_ENV_VALUE" WANDB_PROJECT="$WANDB_PROJECT_VALUE" CONFIG="$MUJOCO_CONFIG" sbatch --parsable --time="$SLURM_TIME_VALUE" "$ROOT_DIR/scripts_refine/train_refine_mujoco.sh")
 
 echo "Submitted ETTh2 refine finetune job:      $ETTH2_JOB"
 echo "Submitted AirQuality refine finetune job: $AIRQUALITY_JOB"
