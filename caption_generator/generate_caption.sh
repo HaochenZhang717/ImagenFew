f4a757195e5c3475579a1b92af8f4503ddd93b5b
Haochen Zhang <hz112@rice.edu>
2026-04-19 11:16:44 +0800

    0419

--- caption_generator/generate_caption.sh
+++ caption_generator/generate_caption.sh
@@ -1,6 +1,6 @@
 python generate_stage2.py \
-  --checkpoint /playpen-shared/haochenz/ImagenFew/caption_generator/logs/caption_generator/ettm1_stage2_diffusion_prior/stage2_best.pt \
-  --config caption_generator/configs/ettm1_stage2_diffusion_prior.yaml \
+  --checkpoint ./logs/caption_generator/ettm1_stage2_diffusion_prior/stage2_best.pt \
+  --config ./configs/ettm1_stage2_diffusion_prior.yaml \
   --use-ema \
   --num-samples 8 \
   --retrieve-train \
