d8636b7ae8f5105caf7e6a7a605aad9c16a4b145
Haochen Zhang <hz112@rice.edu>
2026-04-19 13:58:41 +0800

    0419

--- caption_generator/submit_stage1.sh
+++ caption_generator/submit_stage1.sh
@@ -1,6 +1,6 @@
-#sbatch --export=ALL,DATASET=ettm1 slurm_stage1.sh
-sbatch --export=ALL,DATASET=istanbul slurm_stage1.sh
-sbatch --export=ALL,DATASET=synthetic_u slurm_stage1.sh
-#sbatch --export=ALL,DATASET=synthetic_m slurm_stage1.sh
-#sbatch --export=ALL,DATASET=blindways slurm_stage1.sh
-#sbatch --export=ALL,DATASET=weather slurm_stage1.sh
+#CONDA_ENV=vlm sbatch --export=ALL,DATASET=ettm1 slurm_stage1.sh
+CONDA_ENV=vlm sbatch --export=ALL,DATASET=istanbul slurm_stage1.sh
+CONDA_ENV=vlm sbatch --export=ALL,DATASET=synthetic_u slurm_stage1.sh
+#CONDA_ENV=vlm sbatch --export=ALL,DATASET=synthetic_m slurm_stage1.sh
+#CONDA_ENV=vlm sbatch --export=ALL,DATASET=blindways slurm_stage1.sh
+#CONDA_ENV=vlm sbatch --export=ALL,DATASET=weather slurm_stage1.sh
