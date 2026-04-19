9966e695fc734474f1acffc3bbd6f11f2fe75cf5
Haochen Zhang <hz112@rice.edu>
2026-04-19 11:13:48 +0800

    0419

--- caption_generator/generate_caption.sh
+++ caption_generator/generate_caption.sh
@@ -7,7 +7,7 @@ python caption_generator/generate_stage2.py \
   --retrieve-train-topk 5
 
 
-python caption_generator/generate_stage2.py \
-  --checkpoint /playpen-shared/haochenz/ImagenFew/caption_generator/logs/caption_generator/ettm1_stage2_diffusion_prior/stage2_best.pt \
-  --config caption_generator/configs/ettm1_stage2_diffusion_prior.yaml \
-  --use-ema
+#python caption_generator/generate_stage2.py \
+#  --checkpoint /playpen-shared/haochenz/ImagenFew/caption_generator/logs/caption_generator/ettm1_stage2_diffusion_prior/stage2_best.pt \
+#  --config caption_generator/configs/ettm1_stage2_diffusion_prior.yaml \
+#  --use-ema
