b92d6fdcb01a34fa39d30983a4426c7878926bea
Haochen Zhang <hz112@rice.edu>
2026-04-20 23:08:33 +0800

    0420

--- VerbalTS/calculate_fid_synthu.sh
+++ VerbalTS/calculate_fid_synthu.sh
@@ -18,3 +18,18 @@ python calculate_fid.py \
     --num_heads 8 \
     --latent_dim 64 \
     --save_path "./fid_results/verbal_ts.txt"
+
+
+
+REAL_PATH="/playpen-shared/haochenz/ImagenFew/logs/ImagenTime/VerbalTS_synthetic_u/a8bbb05c-3e69-4a54-8844-ed6ba3b00b66/sample_test.pt"
+FAKE_PATH="/playpen-shared/haochenz/ImagenFew/logs/ImagenTime/VerbalTS_synthetic_u/a8bbb05c-3e69-4a54-8844-ed6ba3b00b66/sample_test.pt"
+python calculate_fid.py \
+    --real_path ${REAL_PATH} \
+    --fake_path ${FAKE_PATH} \
+    --ckpt_path "../fid_vae_ckpts/vae_synth_u/best.pt" \
+    --batch_size 128 \
+    --hidden_size 128 \
+    --num_layers 2 \
+    --num_heads 8 \
+    --latent_dim 64 \
+    --save_path "./fid_results/imagen_time.txt"
