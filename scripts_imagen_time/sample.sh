944f9f58d83bfd8aaa91137be7177191e66503cd
Haochen Zhang <hz112@rice.edu>
2026-04-20 22:54:26 +0800

    0420

--- scripts_imagen_time/sample.sh
+++ scripts_imagen_time/sample.sh
@@ -1,4 +1,4 @@
-python scripts/sample_imagentime_checkpoint.py \
+python scripts_imagen_time/sample_imagentime_checkpoint.py \
   --config configs/ImagenTime/VerbalTS_synthetic_u.yaml \
   --model-ckpt /playpen-shared/haochenz/ImagenFew/logs/ImagenTime/VerbalTS_synthetic_u/a8bbb05c-3e69-4a54-8844-ed6ba3b00b66/ImagenTime.pt \
   --split test \
