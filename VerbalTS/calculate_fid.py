4857385b432827a548a339f91db4cdd66ea3b084
Haochen Zhang <hz112@rice.edu>
2026-04-20 23:11:11 +0800

    0420

--- VerbalTS/calculate_fid.py
+++ VerbalTS/calculate_fid.py
@@ -130,7 +130,7 @@ def load_dataset(dict_path, dict_key, idx=-1):
     if data.shape[1] > data.shape[2]:
         data = data.permute(0,2,1)
     # print(f"Loaded {dict_path}, key: {dict_key}: {data.shape}")
-    print(f"max: {data.max().item()}, min: {data.min().item()}")
+    # print(f"max: {data.max().item()}, min: {data.min().item()}")
     return TensorDataset(data.float())
 
 
