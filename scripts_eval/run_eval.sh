f3f0f01d91d053fc07bf9ae879d214941a100ef3
Haochen Zhang <hz112@rice.edu>
2026-04-21 15:12:02 +0800

    0420

--- scripts_eval/run_eval.sh
+++ scripts_eval/run_eval.sh
@@ -1,11 +1,11 @@
-python scripts/evaluate_saved_eval_samples.py \
+python scripts_eval/evaluate_saved_eval_samples.py \
   --eval-samples-dir /playpen-shared/haochenz/ImagenFew/logs/ImagenTime/VerbalTS_istanbul_traffic/8cb20851-0b7d-4388-8f42-a67b10cd1fc3/eval_samples \
   --eval-metrics disc vaeFID \
   --fid-vae-ckpt-root /playpen-shared/haochenz/ImagenFew/fid_vae_ckpts
 
 
 
-python scripts/evaluate_saved_eval_samples.py \
+python scripts_eval/evaluate_saved_eval_samples.py \
   --eval-samples-dir /playpen-shared/haochenz/ImagenFew/logs/ImagenTimeVectorCond/VerbalTS_istanbul_traffic_qwen3/2d9c363b-2b4d-403a-8ccd-439d581cb0ff/eval_samples \
   --eval-metrics disc vaeFID \
   --fid-vae-ckpt-root /playpen-shared/haochenz/ImagenFew/fid_vae_ckpts
