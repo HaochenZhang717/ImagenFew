88b7411de42403ef47db47f35dbc8ae922a177bd
Haochen Zhang <hz112@rice.edu>
2026-04-21 15:28:06 +0800

    0420

--- scripts_eval/run_eval.sh
+++ scripts_eval/run_eval.sh
@@ -2,8 +2,8 @@ python scripts_eval/evaluate_saved_eval_samples.py \
   --eval-samples-dir /playpen-shared/haochenz/ImagenFew/logs/ImagenTime/VerbalTS_istanbul_traffic/8cb20851-0b7d-4388-8f42-a67b10cd1fc3/eval_samples \
   --eval-metrics disc vaeFID \
   --fid-vae-ckpt-root /playpen-shared/haochenz/ImagenFew/fid_vae_ckpts \
-  --output_json "./ImagenTime_istanbul.json" \
-  --output_csv "./ImagenTime_istanbul.json"
+  --output-json "./ImagenTime_istanbul.json" \
+  --output-csv "./ImagenTime_istanbul.json"
 
 
 
@@ -11,7 +11,7 @@ python scripts_eval/evaluate_saved_eval_samples.py \
   --eval-samples-dir /playpen-shared/haochenz/ImagenFew/logs/ImagenTimeVectorCond/VerbalTS_istanbul_traffic_qwen3/2d9c363b-2b4d-403a-8ccd-439d581cb0ff/eval_samples \
   --eval-metrics disc vaeFID \
   --fid-vae-ckpt-root /playpen-shared/haochenz/ImagenFew/fid_vae_ckpts \
-    --output_json "./ImagenTimeVecCond_istanbul.json" \
-  --output_csv "./ImagenTimeVecCond_istanbul.json"
+  --output-json "./ImagenTimeVecCond_istanbul.json" \
+  --output-csv "./ImagenTimeVecCond_istanbul.json"
 
 
