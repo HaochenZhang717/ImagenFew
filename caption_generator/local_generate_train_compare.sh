749dbdb25c0f9e2f9c24fcb4097a3749d373e299
Haochen Zhang <hz112@rice.edu>
2026-04-22 19:02:36 +0800

    0422

--- caption_generator/local_generate_train_compare.sh
+++ caption_generator/local_generate_train_compare.sh
@@ -1,5 +1,12 @@
+#DATASET=ettm1 \
+#SPLIT=train \
+#MAX_SAMPLES=20 \
+#CHECKPOINT=/playpen-shared/haochenz/ImagenFew/caption_generator/logs/caption_generator/ettm1_stage1_qwen25_3b/joint_caption_best.pt \
+#bash generate_caption_train_compare.sh
+
+
 DATASET=ettm1 \
 SPLIT=train \
 MAX_SAMPLES=20 \
-CHECKPOINT=/playpen-shared/haochenz/ImagenFew/caption_generator/logs/caption_generator/ettm1_stage1_qwen25_3b/joint_caption_best.pt \
+CHECKPOINT=/playpen-shared/haochenz/ImagenFew/caption_generator/logs/caption_generator/ettm1_stage1_qwen25_3b/joint_caption_last.pt \
 bash generate_caption_train_compare.sh
