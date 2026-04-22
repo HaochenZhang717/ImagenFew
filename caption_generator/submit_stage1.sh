052faec887df1821f63d4d7a7677aeabc1d68e3f
Haochen Zhang <hz112@rice.edu>
2026-04-22 14:54:51 +0800

    0421

--- caption_generator/submit_stage1.sh
+++ caption_generator/submit_stage1.sh
@@ -1,4 +1,4 @@
-CONDA_ENV=vlm sbatch --export=ALL,DATASET=ettm1,NPROC_PER_NODE=4 slurm_stage1.sh
+CONDA_ENV=vlm sbatch --export=ALL,DATASET=ettm1,NPROC_PER_NODE=1 slurm_stage1.sh
 #CONDA_ENV=vlm sbatch --export=ALL,DATASET=istanbul,NPROC_PER_NODE=1 slurm_stage1.sh
 #CONDA_ENV=vlm sbatch --export=ALL,DATASET=synthetic_u,NPROC_PER_NODE=1 slurm_stage1.sh
 #CONDA_ENV=vlm sbatch --export=ALL,DATASET=synthetic_m,NPROC_PER_NODE=1 slurm_stage1.sh
