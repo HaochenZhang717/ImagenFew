c7885b8343081dbf12dd51e9d8db75547b6601ef
Haochen Zhang <hz112@rice.edu>
2026-04-22 18:43:46 +0800

    0422

---
+++ caption_generator/local_generate_train_compare.sh
@@ -0,0 +1,5 @@
+DATASET=ettm1 \
+SPLIT=train \
+MAX_SAMPLES=20 \
+CHECKPOINT=/playpen-shared/haochenz/ImagenFew/caption_generator/logs/caption_generator/ettm1_stage1_qwen25_3b/joint_caption_best.pt \
+bash generate_caption_train_compare.sh
