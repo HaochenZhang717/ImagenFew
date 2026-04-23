ce247f7d909c868a44df97666365926b1c33486c
Haochen Zhang <hz112@rice.edu>
2026-04-23 18:42:49 +0800

    0423

--- scripts/run_caption_400100.sh
+++ scripts/run_caption_400100.sh
@@ -2,5 +2,5 @@ HF_HOME_DEFAULT="$HOME/.cache/huggingface"
 export HF_HOME="${HF_HOME:-$HF_HOME_DEFAULT}"
 export TOKENIZERS_PARALLELISM="${TOKENIZERS_PARALLELISM:-false}"
 
-python scripts/run_caption_400x100.py --split test
+python scripts/run_caption_400x100.py --split train
 
