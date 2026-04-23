1cd70416ac89d2f319711295996e4a990a99b644
Haochen Zhang <hz112@rice.edu>
2026-04-23 16:38:08 +0800

    0423

---
+++ caption_generator/pipeline_v2_train.sh
@@ -0,0 +1,6 @@
+HF_HOME_DEFAULT="$HOME/.cache/huggingface"
+export HF_HOME="${HF_HOME:-$HF_HOME_DEFAULT}"
+export TOKENIZERS_PARALLELISM="${TOKENIZERS_PARALLELISM:-false}"
+
+
+CUDA_VISIBLE_DEVICES=3 python train_pipeline_v2.py --config configs/ettm1_pipeline_v2_qwen3vl.yaml
