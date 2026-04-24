b299f0334919afa0a2d43ff6fc3d8f1b50ece3dc
Haochen Zhang <hz112@rice.edu>
2026-04-24 12:40:21 +0800

    0324

--- models/DiT/dit_model.py
+++ models/DiT/dit_model.py
@@ -393,10 +393,15 @@ class FrozenTextEncoder(nn.Module):
         input_ids = encoded["input_ids"].to(device=device, dtype=torch.long)
         attention_mask = encoded["attention_mask"].to(device=device, dtype=torch.long)
         model_output = self.forward(input_ids=input_ids, attention_mask=attention_mask)
-        sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+        sentence_embeddings = self.mean_pooling(model_output, encoded_input['attention_mask'])
         sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=1)
 
         return sentence_embeddings
+
+    def mean_pooling(self, model_output, attention_mask):
+        token_embeddings = model_output[0]  # First element of model_output contains all token embeddings
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
 
 class StyleEmbedder(nn.Module):
