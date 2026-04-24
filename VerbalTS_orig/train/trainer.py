ac3b3b4e86de9eebcb973dbd42a7139c2e8e439d
Haochen Zhang <hz112@rice.edu>
2026-04-24 15:26:35 +0800

    0424

--- VerbalTS_orig/train/trainer.py
+++ VerbalTS_orig/train/trainer.py
@@ -3,10 +3,10 @@ import time
 import numpy as np
 import torch
 from torch.optim import Adam
-from torch.utils.tensorboard import SummaryWriter
 from data import GenerationDataset
 from evaluation.base_evaluator import BaseEvaluator
-import matplotlib.pyplot as plt
+import wandb
+
 
 class Trainer:
     """
@@ -19,7 +19,18 @@ class Trainer:
         self._init_data(dataset)
         self._init_eval(eval_configs)
         self._best_valid_loss = 1e10
-        self.tf_writer = SummaryWriter(log_dir=self.output_folder)
+
+        wandb.init(
+            project="verbalts_0424",  # 你可以改名字
+            name=os.getenv("WANDB_NAME", "no_name"),  # 自动用实验名
+            config=self.configs
+        )
+
+    def update_ema(self):
+        with torch.no_grad():
+            for ema_param, param in zip(self.ema_model.parameters(), self.model.parameters()):
+                ema_param.data.mul_(self.ema_decay).add_(param.data, alpha=1 - self.ema_decay)
+
 
     def _init_eval(self, eval_configs):
         dataset = GenerationDataset(eval_configs["data"])
@@ -43,16 +54,34 @@ class Trainer:
 
     def _init_model(self, model):
         self.model = model
+        self.ema_model = copy.deepcopy(self.model)
         if self.model_path != "":
             print("Loading pretrained model from {}".format(self.model_path))
-            self.model.load_state_dict(torch.load(self.model_path))
+            ckpt = torch.load(self.model_path)
+            if "ema_model" in ckpt:
+                self.model.load_state_dict(ckpt["model"])
+                self.ema_model.load_state_dict(ckpt["ema_model"])
+            else:
+                self.model.load_state_dict(ckpt)
+
+        self.ema_model.eval()
+        self.ema_decay = 0.999  # 可以调 0.999~0.9999
+
+        total_params = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+        print(f"Trainable parameters: {total_params}")
+
 
     def _init_opt(self):
         self.opt = Adam(self.model.parameters(), lr=self.lr, weight_decay=1e-6)
-        p1 = int(0.75 * self.n_epochs)
-        p2 = int(0.9 * self.n_epochs)
-        self.lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
-            self.opt, milestones=[p1, p2], gamma=0.1)
+        self.lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            self.opt,
+            T_max=self.n_epochs,
+            eta_min=0.1 * self.lr
+        )
+        # p1 = int(0.75 * self.n_epochs)
+        # p2 = int(0.9 * self.n_epochs)
+        # self.lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+        #     self.opt, milestones=[p1, p2], gamma=0.1)
 
     def _init_data(self, dataset):
         self.dataset = dataset
@@ -67,20 +96,21 @@ class Trainer:
     Train.
     """
     def train(self):
+
         self._reset_train()
         for epoch_no in range(self.n_epochs):
             self._train_epoch(epoch_no)
             if self.valid_loader is not None and (epoch_no + 1) % self.valid_epoch_interval == 0:
                 self.valid(epoch_no)
-                self.evaluate(epoch_no)
+                # self.evaluate(epoch_no)
     
     def evaluate(self, epoch_no):
-        self.model.eval()
-        self.evaluator.model = self.model
-        self.evaluator.n_samples = 10
-        res_dict = self.evaluator.evaluate(mode="cond_gen", sampler="ddim", save_pred=False)
-        for k in res_dict["tensorboard"].keys():
-            self.tf_writer.add_scalar(fr"Cond_gen/{k}", res_dict["tensorboard"][k], epoch_no)
+        self.ema_model.eval()
+        self.evaluator.model = self.ema_model
+        self.evaluator.n_samples = 1
+        # res_dict = self.evaluator.evaluate(mode="cond_gen", sampler="ddim", save_pred=False)
+        # for k in res_dict["tensorboard"].keys():
+        #     self.tf_writer.add_scalar(fr"Cond_gen/{k}", res_dict["tensorboard"][k], epoch_no)
         
     def _train_epoch(self, epoch_no):
             start_time = time.time()
@@ -93,18 +123,24 @@ class Trainer:
                 loss_dict = self.model(train_batch, is_train=True)
 
                 loss_dict["all"].backward()
+
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
                 self.opt.step()
+                self.update_ema()
 
                 avg_loss += loss_dict["all"].item()
-                for k in loss_dict.keys():
-                    self.tf_writer.add_scalar(fr"Train/{k}", loss_dict[k].item(), self._global_batch_no)
 
                 if batch_no >= self.itr_per_epoch:
                     break
             self.lr_scheduler.step()
             avg_loss /= len(self.train_loader)
-            self.tf_writer.add_scalar("Train/epoch_loss", avg_loss, epoch_no)
-            self.tf_writer.add_scalar("Train/lr", self.opt.param_groups[0]['lr'], epoch_no)
+
+            wandb.log({
+                "train/epoch_loss": avg_loss,
+                "train/learning_rate": self.opt.param_groups[0]['lr'],
+                "epoch": epoch_no
+            }, step=epoch_no)
+
             end_time = time.time()
             
             if (epoch_no+1)%self.display_epoch_interval==0:
@@ -116,7 +152,7 @@ class Trainer:
     Valid.
     """
     def valid(self, epoch_no=-1):
-        self.model.eval()
+        self.ema_model.eval()
         avg_loss_valid = 0
         with torch.no_grad():
             for batch_no, valid_batch in enumerate(self.valid_loader):
@@ -124,8 +160,10 @@ class Trainer:
                 avg_loss_valid += loss_dict["all"].item()
 
         avg_loss_valid = avg_loss_valid/len(self.valid_loader)
-        self.tf_writer.add_scalar("Valid/epoch_loss", avg_loss_valid, epoch_no)
-
+        wandb.log({
+            "valid/loss": avg_loss_valid,
+            "epoch": epoch_no
+        }, step=epoch_no)
         if self._best_valid_loss > avg_loss_valid:
             self._best_valid_loss = avg_loss_valid
             print(f"\n*** Best loss is updated to {avg_loss_valid} at {epoch_no}.\n")
@@ -138,4 +176,9 @@ class Trainer:
     def save_model(self, comment):
         os.makedirs(fr"{self.output_folder}/ckpts", exist_ok=True)
         path = os.path.join(fr"{self.output_folder}/ckpts", f"{comment}.pth")
-        torch.save(self.model.state_dict(), path)
+        torch.save({
+            "model": self.model.state_dict(),
+            "ema_model": self.ema_model.state_dict(),
+            "comment": comment,
+        }, path)
+
