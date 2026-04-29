891091681df4e8d20e95fade8416d326d404942c
Haochen Zhang <hz112@rice.edu>
2026-04-29 21:35:19 +0800

    0429

--- VerbalTS_orig/scripts/istanbul_traffic/train_image_local.sh
+++ VerbalTS_orig/scripts/istanbul_traffic/train_image_local.sh
@@ -1,24 +1,7 @@
-#python run_image.py \
-#  --cond_modal text \
-#  --training_stage finetune \
-#  --save_folder ./logs/istanbul_traffic_image_augmented/text2ts_segment \
-#  --model_diff_config_path configs/istanbul_traffic/diff/model_text2ts_dep.yaml \
-#  --model_cond_config_path configs/istanbul_traffic/cond/text_msmdiffmv.yaml \
-#  --train_config_path configs/istanbul_traffic/train.yaml \
-#  --evaluate_config_path configs/istanbul_traffic/evaluate.yaml \
-#  --data_folder ../data/VerbalTSDatasets/istanbul_traffic \
-#  --img_size 14 \
-#  --patch_size 2 \
-#  --epochs 1000 \
-#  --batch_size 512 \
-#  --n_runs 1 \
-#  --only_evaluate True
-
-
 python run_image.py \
   --cond_modal text \
   --training_stage finetune \
-  --save_folder ./logs/istanbul_traffic_image/text2ts_segment \
+  --save_folder ./logs/istanbul_traffic_image_augmented/text2ts_segment \
   --model_diff_config_path configs/istanbul_traffic/diff/model_text2ts_dep.yaml \
   --model_cond_config_path configs/istanbul_traffic/cond/text_msmdiffmv.yaml \
   --train_config_path configs/istanbul_traffic/train.yaml \
@@ -30,3 +13,20 @@ python run_image.py \
   --batch_size 512 \
   --n_runs 1 \
   --only_evaluate True
+
+
+#python run_image.py \
+#  --cond_modal text \
+#  --training_stage finetune \
+#  --save_folder ./logs/istanbul_traffic_image/text2ts_segment \
+#  --model_diff_config_path configs/istanbul_traffic/diff/model_text2ts_dep.yaml \
+#  --model_cond_config_path configs/istanbul_traffic/cond/text_msmdiffmv.yaml \
+#  --train_config_path configs/istanbul_traffic/train.yaml \
+#  --evaluate_config_path configs/istanbul_traffic/evaluate.yaml \
+#  --data_folder ../data/VerbalTSDatasets/istanbul_traffic \
+#  --img_size 14 \
+#  --patch_size 2 \
+#  --epochs 1000 \
+#  --batch_size 512 \
+#  --n_runs 1 \
+#  --only_evaluate True
